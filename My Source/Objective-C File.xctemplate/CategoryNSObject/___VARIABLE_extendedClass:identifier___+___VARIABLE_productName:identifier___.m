697c4f4f946a34063dd3109921a444783a74c146
Alexandr Kozin <al.kozin@gmail.com>
2015-09-17 17:48:33 +0600

    Initial

---
+++ My Source/Objective-C File.xctemplate/CategoryNSObject/___VARIABLE_extendedClass:identifier___+___VARIABLE_productName:identifier___.m
@@ -0,0 +1,17 @@
+//
+//  ___VARIABLE_extendedClass:identifier___+___VARIABLE_productName:identifier___.m
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___VARIABLE_extendedClass:identifier____Private.h"
+
+@interface ___VARIABLE_extendedClass:identifier___ (___VARIABLE_productName:identifier___)
+
+@end
+
+@implementation ___VARIABLE_extendedClass:identifier___ (___VARIABLE_productName:identifier___)
+
+@end
